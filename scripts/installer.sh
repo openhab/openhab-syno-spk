78d59fe4cb11674172989b25d9759ee20422c8bb
Christian Häussler <c-n-i@web.de>
2017-07-18 00:08:25 +0200

    Update to version 2.1.0.005
    Signed-off-by: Christian Häussler <c-n-i@web.de> (github: @cniweb)

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#--------OpenHAB installer script
+#--------openHAB2 installer script
 #--------package based on work from pcloadletter.co.uk
 
 DOWNLOAD_PATH="https://bintray.com/openhab/mvn/download_file?file_path=org/openhab/distro/openhab/2.1.0"
@@ -15,6 +15,7 @@ DAEMON_USER="$(echo ${SYNOPKG_PKGNAME} | awk {'print tolower($_)'})"
 DAEMON_PASS="$(openssl rand 12 -base64 2>nul)"
 DAEMON_ID="${SYNOPKG_PKGNAME} daemon user"
 ENGINE_SCRIPT="start.sh"
+PIDFILE="/var/services/homes/${DAEMON_USER}/.daemon.pid"
 
 source /etc/profile
 
@@ -22,6 +23,9 @@ TEMP_FOLDER="$(find / -maxdepth 2 -name '@tmp' | head -n 1)"
 PRIMARY_VOLUME="$(echo ${TEMP_FOLDER} | grep -oP '^/[^/]+')"
 PUBLIC_FOLDER="$(synoshare --get public | grep -oP 'Path.+\[\K[^]]+')"
 
+PUBLIC_CONF="${PUBLIC_FOLDER}/openHAB2/conf"
+PUBLIC_ADDONS="${PUBLIC_FOLDER}/openHAB2/addons"
+TIMESTAMP=`date +%Y%m%d_%H%M%S`;
 
 preinst ()
 {
@@ -40,6 +44,13 @@ preinst ()
     exit 1
   fi
 
+  synoshare -get public > /dev/null || (
+    echo "A shared folder called 'public' could not be found - note this name is case-sensitive. "
+    echo "Please create this using the Shared Folder DSM Control Panel and try again."
+    exit 1
+  )
+
+  echo "Get new version"
   cd ${TEMP_FOLDER}
   # go through list of files
   for WGET_URL in ${INSTALL_FILES}; do
@@ -71,8 +82,13 @@ preinst ()
 postinst ()
 {
   #create daemon user
+  echo "Create '${DAEMON_USER}' daemon user"
   synouser --add ${DAEMON_USER} ${DAEMON_PASS} "${DAEMON_ID}" 0 "" ""
   sleep 3
+
+  #add openhab user & handle possible device groups
+  synogroup --member dialout ${DAEMON_USER}
+  synogroup --member uucp ${DAEMON_USER}
 
   #determine the daemon user homedir and save that variable in the user's profile
   #this is needed because new users seem to inherit a HOME value of /root which they have no permissions for
@@ -81,14 +97,38 @@ postinst ()
   su - ${DAEMON_USER} -s /bin/sh -c "echo export OPENHAB_PID=~/.daemon.pid >> .profile"
 
   #extract main archive
+  echo "Install new version"
   cd ${TEMP_FOLDER}
-  7z x ${TEMP_FOLDER}/${DOWNLOAD_FILE1} -o${EXTRACTED_FOLDER} && rm ${TEMP_FOLDER}/${DOWNLOAD_FILE1}
+  7z x ${TEMP_FOLDER}/${DOWNLOAD_FILE1} -o ${EXTRACTED_FOLDER} && rm ${TEMP_FOLDER}/${DOWNLOAD_FILE1}
   mv ${TEMP_FOLDER}/${EXTRACTED_FOLDER}/* ${SYNOPKG_PKGDEST}
   rmdir ${TEMP_FOLDER}/${EXTRACTED_FOLDER}
   chmod +x ${SYNOPKG_PKGDEST}/${ENGINE_SCRIPT}
 
+  echo "create conf/addon links"
+  #if configdir exists in public folder -> create a symbolic link
+  if [ -d ${PUBLIC_CONF} ]; then
+    rm -r ${SYNOPKG_PKGDEST}/conf
+    ln -s ${PUBLIC_CONF} ${SYNOPKG_PKGDEST}
+    chmod -R u+w ${PUBLIC_CONF}
+  fi
+
+  #if public addons dir exists in public folder -> create a symbolic link
+  if [ -d ${PUBLIC_ADDONS} ]; then
+    rm -r ${SYNOPKG_PKGDEST}/addons
+    ln -s ${PUBLIC_ADDONS} ${SYNOPKG_PKGDEST}
+    chmod -R u+w ${PUBLIC_ADDONS}
+  fi
+
+  #add log file
+  mkdir -p ${SYNOPKG_PKGDEST}/userdata/logs
+  touch ${SYNOPKG_PKGDEST}/userdata/logs/openhab.log
+
   #change owner of folder tree
-  chown -R ${DAEMON_USER} ${SYNOPKG_PKGDEST}
+  echo "Fix permssion"
+  chown -hR ${DAEMON_USER} ${PUBLIC_CONF}
+  chown -hR ${DAEMON_USER} ${PUBLIC_ADDONS}
+  chown -hR ${DAEMON_USER} ${SYNOPKG_PKGDEST}
+  chmod -R u+w ${SYNOPKG_PKGDEST}/userdata
 
   #if Z-Wave dir exists -> change rights for binding
   if [ -d /dev/ttyACM0 ]; then
@@ -129,6 +169,111 @@ postuninst ()
   else
     echo "Daemon user folder '${DAEMON_HOME}' not found - nothing deleted"
   fi
+
+  exit 0
+}
+
+
+preupgrade ()
+{
+  #make sure server is stopped
+  echo "Stop server"
+  if su - ${DAEMON_USER} -s /bin/sh -c "cd ${SYNOPKG_PKGDEST}/runtime/karaf/bin && ./stop &"; then
+    rm -f $PIDFILE
+  fi
+  sleep 10
+
+  # Remove tmp, logs & cache dirs
+  if [ -d ${SYNOPKG_PKGDEST}/userdata/tmp ]; then
+  	echo "Remove tmp"
+  	rm -rf ${SYNOPKG_PKGDEST}/userdata/tmp
+  fi
+
+  if [ -d ${SYNOPKG_PKGDEST}/userdata/cache ]; then
+  	echo "Remove cache"
+  	rm -rf ${SYNOPKG_PKGDEST}/userdata/cache
+  fi
+
+  if [ -d ${SYNOPKG_PKGDEST}/userdata/log ]; then
+  	echo "Remove log"
+  	rm -rf ${SYNOPKG_PKGDEST}/userdata/log
+  fi
+
+  if [ -d ${SYNOPKG_PKGDEST}/userdata/logs ]; then
+  	echo "Remove logs"
+  	rm -rf ${SYNOPKG_PKGDEST}/userdata/logs
+  fi
+
+  # backup current installation with settings
+  echo "Backup"
+  mv ${SYNOPKG_PKGDEST} /${SYNOPKG_PKGDEST}-backup-$TIMESTAMP
+
+  echo "Get new version"
+  cd ${TEMP_FOLDER}
+  # go through list of files
+  for WGET_URL in ${INSTALL_FILES}; do
+    WGET_FILENAME="$(echo ${WGET_URL} | sed -r "s%^.*/(.*)%\1%")"
+    echo "Processing ${WGET_FILENAME}"
+    [ -f "${TEMP_FOLDER}/${WGET_FILENAME}" ] && rm ${TEMP_FOLDER}/${WGET_FILENAME}
+    # use local file first
+    if [ -f "${PUBLIC_FOLDER}/${WGET_FILENAME}" ]; then
+      echo "Found file locally - copying"
+      cp ${PUBLIC_FOLDER}/${WGET_FILENAME} ${TEMP_FOLDER}
+    else
+      wget -nv --no-check-certificate --output-document=${WGET_FILENAME} ${WGET_URL}
+      if [[ $? != 0 ]]; then
+          echo "There was a problem downloading ${WGET_FILENAME} from the download link:"
+          echo "'${WGET_URL}'"
+          echo "Alternatively, download this file manually and place it in the 'public' shared folder and start installation again."
+          if [ -z "${PUBLIC_FOLDER}" ];then
+            echo "Note: You must create a 'public' shared folder first on your primary volume"
+          fi
+          exit 1
+      fi
+    fi
+  done
+
+  exit 0
+}
+
+
+postupgrade ()
+{
+  #extract main archive
+  echo "Install new version"
+  cd ${TEMP_FOLDER}
+  7z x ${TEMP_FOLDER}/${DOWNLOAD_FILE1} -o${EXTRACTED_FOLDER} && rm ${TEMP_FOLDER}/${DOWNLOAD_FILE1}
+  mv ${TEMP_FOLDER}/${EXTRACTED_FOLDER}/* ${SYNOPKG_PKGDEST}
+  rmdir ${TEMP_FOLDER}/${EXTRACTED_FOLDER}
+  chmod +x ${SYNOPKG_PKGDEST}/${ENGINE_SCRIPT}
+
+  # restore configuration and userdata
+  echo "Restore UserData"
+  cp -ar ${SYNOPKG_PKGDEST}-backup-$TIMESTAMP/userdata ${SYNOPKG_PKGDEST}/
+
+  echo "Create conf/addon links"
+  #if configdir exists in public folder -> create a symbolic link
+  if [ -d ${PUBLIC_CONF} ]; then
+    rm -r ${SYNOPKG_PKGDEST}/conf
+    ln -s ${PUBLIC_CONF} ${SYNOPKG_PKGDEST}
+    chmod -R u+w ${PUBLIC_CONF}
+  fi
+
+  #if public addons dir exists in public folder -> create a symbolic link
+  if [ -d ${PUBLIC_ADDONS} ]; then
+    rm -r ${SYNOPKG_PKGDEST}/addons
+    ln -s ${PUBLIC_ADDONS} ${SYNOPKG_PKGDEST}
+    chmod -R u+w ${PUBLIC_ADDONS}
+  fi
+
+  #add log file
+  mkdir -p ${SYNOPKG_PKGDEST}/userdata/logs
+  touch ${SYNOPKG_PKGDEST}/userdata/logs/openhab.log
+
+  # fix permissions
+  echo "Fix permssion"
+  chown -hR ${DAEMON_USER} ${SYNOPKG_PKGDEST}
+  chmod -R u+w ${SYNOPKG_PKGDEST}/userdata
 
   exit 0
 }

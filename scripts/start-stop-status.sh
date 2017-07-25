6b3ac7565431038c3eec07bec7d582a8e4943a1c
SchoppAx <develop4all@gmail.com>
2017-07-26 00:03:13 +0200

    Bug fixes and Backup functionality (#79)
    * Fix public folder permissions #62 + #71 + #80 
    * Change 7z to tar for < DSM 6 support
    * Fix Backup functionality #61

--- scripts/start-stop-status.sh
+++ scripts/start-stop-status.sh
@@ -26,13 +26,13 @@ case $1 in
     
     #set the current timezone for Java so that log timestamps are accurate
     #we need to use the modern timezone names so that Java can figure out DST
-    SYNO_TZ=`cat /etc/synoinfo.conf | grep timezone | cut -f2 -d'"'`
-    #fix for DST time in DSM 5.2 thanks to MinimServer Syno package author
-    [ -e /usr/share/zoneinfo/Timezone/synotztable.json ] \
-    && SYNO_TZ=`jq ".${SYNO_TZ} | .nameInTZDB" /usr/share/zoneinfo/Timezone/synotztable.json | sed -e "s/\"//g"` \
-    || SYNO_TZ=`grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzname | sed -e "s/^.*= //"`
-    #Before DSM 5.1
-    #SYNO_TZ=grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzlist | sed -e "s/^.*= //"
+    if [ -n "$SYNOPKG_DSM_VERSION_MAJOR" -a $SYNOPKG_DSM_VERSION_MAJOR -ge 6 ]; then
+      SYNO_TZ=$(cat /etc/synoinfo.conf | grep timezone | cut -f2 -d'"')
+    else
+      [ -e /usr/share/zoneinfo/Timezone/synotztable.json ] \
+      && SYNO_TZ=$(jq ".${SYNO_TZ} | .nameInTZDB" /usr/share/zoneinfo/Timezone/synotztable.json | sed -e "s/\"//g") \
+      || SYNO_TZ=$(grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzname | sed -e "s/^.*= //")
+    fi  
     grep "^export TZ" ${DAEMON_HOME}/.profile > /dev/null \
      && sed -i "s%^export TZ=.*$%export TZ='${SYNO_TZ}'%" ${DAEMON_HOME}/.profile \
      || echo export TZ=\'${SYNO_TZ}\' >> ${DAEMON_HOME}/.profile

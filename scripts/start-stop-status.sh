541f5338c58a3b0180d7559a77ee6971d5dabe68
SchoppAx <develop4all@gmail.com>
2017-07-27 00:26:51 +0200

    Update start-stop-status.sh (#82)
    - Fix TimeZone bug for all DSM versions

--- scripts/start-stop-status.sh
+++ scripts/start-stop-status.sh
@@ -26,12 +26,13 @@ case $1 in
     
     #set the current timezone for Java so that log timestamps are accurate
     #we need to use the modern timezone names so that Java can figure out DST
+    SYNO_TZ=$(cat /etc/synoinfo.conf | grep timezone | cut -f2 -d'"')
     if [ -n "$SYNOPKG_DSM_VERSION_MAJOR" -a $SYNOPKG_DSM_VERSION_MAJOR -ge 6 ]; then
-      SYNO_TZ=$(cat /etc/synoinfo.conf | grep timezone | cut -f2 -d'"')
+      SYNO_TZ=$(jq ".${SYNO_TZ} | .nameInTZDB" /usr/share/zoneinfo/Timezone/synotztable.json | sed -e "s/\"//g")
+    elif [ -n "$SYNOPKG_DSM_VERSION_MAJOR" -a $SYNOPKG_DSM_VERSION_MAJOR -ge 5 -a $SYNOPKG_DSM_VERSION_MINOR -ge 2 ]; then
+      SYNO_TZ=$(grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzname | sed -e "s/^.*= //")
     else
-      [ -e /usr/share/zoneinfo/Timezone/synotztable.json ] \
-      && SYNO_TZ=$(jq ".${SYNO_TZ} | .nameInTZDB" /usr/share/zoneinfo/Timezone/synotztable.json | sed -e "s/\"//g") \
-      || SYNO_TZ=$(grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzname | sed -e "s/^.*= //")
+      SYNO_TZ=$(grep "^${SYNO_TZ}" /usr/share/zoneinfo/Timezone/tzlist | sed -e "s/^.*= //")
     fi  
     grep "^export TZ" ${DAEMON_HOME}/.profile > /dev/null \
      && sed -i "s%^export TZ=.*$%export TZ='${SYNO_TZ}'%" ${DAEMON_HOME}/.profile \

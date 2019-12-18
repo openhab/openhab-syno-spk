28c93adcd65488182845005544952b08c96cea26
cniweb <c-n-i@web.de>
2019-12-18 14:57:07 +0100

    Add new Version 3.0.0-SNAPSHOT
    Signed-off-by: cniweb <c-n-i@web.de>

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -11,13 +11,13 @@ echo "$(date +%Y-%m-%d:%H:%M:%S)" >>$LOG
 echo "" >>$LOG
 
 echo "Set instance variables..." >>$LOG
-DOWNLOAD_PATH="https://bintray.com/openhab/mvn/download_file?file_path=org/openhab/distro/openhab/2.5.0"
-DOWNLOAD_FILE1="openhab-2.5.0.zip"
+DOWNLOAD_PATH="https://ci.openhab.org/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab/target"
+DOWNLOAD_FILE1="openhab-3.0.0-SNAPSHOT.zip"
 
 # Add more files by separating them using spaces
 INSTALL_FILES="${DOWNLOAD_PATH}/${DOWNLOAD_FILE1}"
 
-EXTRACTED_FOLDER="openHAB-2.5.0"
+EXTRACTED_FOLDER="openHAB-3.0.0-SNAPSHOT"
 
 DAEMON_USER="$(echo ${SYNOPKG_PKGNAME} | awk {'print tolower($_)'})"
 DAEMON_PASS="$(openssl rand 12 -base64 2>null)"

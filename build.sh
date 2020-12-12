e681d233fd641dfe225f802bee8ce3f1a3113d40
Thomas Karl Pietrowski <thopiekar@gmail.com>
2020-12-12 21:06:04 +0100

    CI: Adding separate build script
    It will allow us to make builds locally the same way as Jenkins does. Easily..
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eux
+
+[[ -z "${OPENHAB_VERSION:-}" ]] && echo "OPENHAB_VERSION is not set!" && exit 1
+
+FILE_NAME=openHAB-${OPENHAB_VERSION}-syno-noarch-0.001.spk
+echo ${FILE_NAME}
+
+chmod a+x scripts/*
+
+rm -f ${FILE_NAME}
+
+tar cf ${FILE_NAME} \
+    --exclude=.git* \
+    --exclude=\*.md \
+    --exclude=.travis.\* \
+    --owner=0 \
+    --group=0 \
+    --numeric-owner \
+    *

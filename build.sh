f9855e0d1c886b0f57c4a5686d00a37297381005
Thomas Karl Pietrowski <thopiekar@gmail.com>
2020-12-24 20:25:07 +0100

    build: Commonizing download, verify and unpack method of Java
    Making it more maintainable and saving lines of code.
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@
 
 # OpenJDK
 OPENJDK_DOWNLOAD_URL_X64="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_x64_linux_hotspot_11.0.9.1_1.tar.gz"
-OPENJDK_DOWNLOAD_FILENAME_X64="$(basename ${OPENJDK_DOWNLOAD_URL_X64})"
 OPENJDK_DOWNLOAD_SHA256_X64="73ce5ce03d2efb097b561ae894903cdab06b8d58fbc2697a5abe44ccd8ecc2e5"
 
-OPENJDK_DOWNLOAD_URL_ARM="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_arm_linux_hotspot_11.0.9.1_1.tar.gz"
-OPENJDK_DOWNLOAD_FILENAME_ARM="$(basename ${OPENJDK_DOWNLOAD_URL_ARM})"
-OPENJDK_DOWNLOAD_SHA256_ARM="11628830c3c912edd10b91620ef0b9566640c5ea46439f1d028f3ebd98682dbb"
+OPENJDK_DOWNLOAD_URL_ARM32="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_arm_linux_hotspot_11.0.9.1_1.tar.gz"
+OPENJDK_DOWNLOAD_SHA256_ARM32="11628830c3c912edd10b91620ef0b9566640c5ea46439f1d028f3ebd98682dbb"
 
 # OpenHAB
 OPENHAB_DOWNLOAD_URLBASE='https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab'
@@ -24,60 +22,50 @@ mkdir -p $DL_DIR
 mkdir -p $BUILD_DIR
 rm -rfv $BUILD_DIR/*
 
-# Java X64: Download
-[[ -e "${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_X64}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document=${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_X64} ${OPENJDK_DOWNLOAD_URL_X64}
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Downloading Java failed with code $returncode."
-    exit 1
-fi
+function download_and_extract_java
+{
+    local download_url=$1
+    local download_checksum=$2
+    local download_extract_dir=$3
+    
+    local download_filename="$(basename ${download_url})"
+    local download_filepath="${DL_DIR}/${download_filename}"
+    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
 
-# Java X64: Verify
-echo "${OPENJDK_DOWNLOAD_SHA256_X64} ${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_X64}" | sha256sum --check --status
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Verifying Java failed with code $returncode."
-    exit 1
-fi
+    # Java X64: Download
+    [[ -e "${download_filepath}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document="${download_filepath}" ${download_url}
+    returncode=$?
+    if [[ $returncode != 0 ]]; then
+        echo " Downloading Java failed with: $returncode"
+        exit 1
+    fi
+    
+    # Java X64: Verify
+    echo "${download_checksum} ${download_filepath}" | sha256sum --check --status || returncode=$?
+    if [[ $returncode != 0 ]]; then
+        echo " Checksum is: $(sha256sum ${download_filepath})"
+        echo " Verifying Java failed with: $returncode"
+        exit 1
+    fi
 
-# Java X64: Unpack
-mkdir -p ${BUILD_DIR}/OpenJDK_X64
-tar --strip-components=1 --directory=${BUILD_DIR}/OpenJDK_X64 -xvzf ${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_X64}
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Installation of OpenJDK with code $returncode failed."
-    exit 1;
-fi
+    # Java X64: Unpack
+    mkdir -p ${download_extract_dirpath}
+    tar --strip-components=1 --directory=${download_extract_dirpath} -xvzf ${download_filepath}
+    returncode=$?
+    if [[ $returncode != 0 ]]; then
+        echo " Installation of OpenJDK with code $returncode failed."
+        exit 1;
+    fi
+    
+}
+
+download_and_extract_java ${OPENJDK_DOWNLOAD_URL_X64} ${OPENJDK_DOWNLOAD_SHA256_X64} "OpenJDK_X64"
+download_and_extract_java ${OPENJDK_DOWNLOAD_URL_ARM32} ${OPENJDK_DOWNLOAD_SHA256_ARM32} "OpenJDK_ARM32"
 
 # Java X64: Linking
 ln -s ./OpenJDK_X64 ${BUILD_DIR}/OpenJDK_x86_64
 
-# Java ARM: Download
-[[ -e "${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_ARM}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document=${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_ARM} ${OPENJDK_DOWNLOAD_URL_ARM}
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Downloading Java failed with code $returncode."
-    exit 1
-fi
-
-# Java ARM: Verify
-echo "${OPENJDK_DOWNLOAD_SHA256_ARM} ${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_ARM}" | sha256sum --check --status
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Verifying Java failed with code $returncode."
-    exit 1
-fi
-
-# Java ARM: Unpack
-mkdir -p ${BUILD_DIR}/OpenJDK_ARM
-tar --strip-components=1 --directory=${BUILD_DIR}/OpenJDK_ARM -xvzf ${DL_DIR}/${OPENJDK_DOWNLOAD_FILENAME_ARM}
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Installation of OpenJDK with code $returncode failed."
-    exit 1;
-fi
-
-# Java ARM: Linking
+# Java ARM32: Linking
 
 
 # Downloading openHAB

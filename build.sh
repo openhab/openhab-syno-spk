b2b009e51c9fd4e7201aaf162287ed5ed7337b62
Thomas Karl Pietrowski <thopiekar@gmail.com>
2020-12-26 21:32:42 +0100

    build: Simplify download and unpack
    Using AdoptOpenJDK as it provides us x64 and arm runtime builds. Zulu doesn't come with arm builds.
    Therefore using builds from one distributor for all architectures.
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- build.sh
+++ build.sh
@@ -1,38 +1,36 @@
 #!/bin/bash -eux
 
 [[ -z "${OPENHAB_VERSION:-}" ]] && echo "OPENHAB_VERSION is not set!" && exit 1
+[[ -z "${SPK_VERSION:-}" ]] && echo "SPK_VERSION is not set!" && exit 1
 
 # OpenJDK
 OPENJDK_DOWNLOAD_URL_X64="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_x64_linux_hotspot_11.0.9.1_1.tar.gz"
-OPENJDK_DOWNLOAD_SHA256_X64="73ce5ce03d2efb097b561ae894903cdab06b8d58fbc2697a5abe44ccd8ecc2e5"
+OPENJDK_DOWNLOAD_CHECKSUM_X64="73ce5ce03d2efb097b561ae894903cdab06b8d58fbc2697a5abe44ccd8ecc2e5"
 
 OPENJDK_DOWNLOAD_URL_ARM32="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_arm_linux_hotspot_11.0.9.1_1.tar.gz"
-OPENJDK_DOWNLOAD_SHA256_ARM32="11628830c3c912edd10b91620ef0b9566640c5ea46439f1d028f3ebd98682dbb"
+OPENJDK_DOWNLOAD_CHECKSUM_ARM32="11628830c3c912edd10b91620ef0b9566640c5ea46439f1d028f3ebd98682dbb"
 
 # OpenHAB
-OPENHAB_DOWNLOAD_URLBASE='https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab'
-OPENHAB_RELEASE="2.5.11"
-OPENHAB_DOWNLOAD_URL="https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/${OPENHAB_RELEASE}/openhab-${OPENHAB_RELEASE}.zip"
+OPENHAB_DOWNLOAD_URL="https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/${OPENHAB_VERSION}/openhab-${OPENHAB_VERSION}.zip"
+OPENHAB_DOWNLOAD_CHECKSUM="NONE"
 OPENHAB_DOWNLOAD_FILENAME="$(basename ${OPENHAB_DOWNLOAD_URL})"
 
 # Ensure empty build directory
 DL_DIR=./dl
 BUILD_DIR=./build
 mkdir -p $DL_DIR
-mkdir -p $BUILD_DIR
-rm -rfv $BUILD_DIR/*
+mkdir -p ${BUILD_DIR}
+rm -rfv ${BUILD_DIR}/*
 
-function download_and_extract_java
+function download_and_verify
 {
     local download_url=$1
     local download_checksum=$2
-    local download_extract_dir=$3
     
     local download_filename="$(basename ${download_url})"
     local download_filepath="${DL_DIR}/${download_filename}"
-    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
 
-    # Java X64: Download
+    # Download
     [[ -e "${download_filepath}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document="${download_filepath}" ${download_url}
     returncode=$?
     if [[ $returncode != 0 ]]; then
@@ -40,57 +38,83 @@ function download_and_extract_java
         exit 1
     fi
     
-    # Java X64: Verify
-    echo "${download_checksum} ${download_filepath}" | sha256sum --check --status || returncode=$?
-    if [[ $returncode != 0 ]]; then
-        echo " Checksum is: $(sha256sum ${download_filepath})"
-        echo " Verifying Java failed with: $returncode"
-        exit 1
+    # Verify
+    if [[ ${download_checksum} != "NONE" ]]; then
+        echo "${download_checksum} ${download_filepath}" | sha256sum --check --status || returncode=$?
+        if [[ $returncode != 0 ]]; then
+            echo " Checksum is: $(sha256sum ${download_filepath})"
+            echo " Verifying Java failed with: $returncode"
+            exit 1
+        fi
     fi
+}
 
-    # Java X64: Unpack
+function download_and_extract_tgz
+{
+    local download_url=$1
+    local download_checksum=$2
+    local download_extract_dir=$3
+
+    local download_filename="$(basename ${download_url})"
+    local download_filepath="${DL_DIR}/${download_filename}"
+    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
+
+    # Download and verify
+    download_and_verify $1 $2
+
+    # Unpack the downloaded tgz
     mkdir -p ${download_extract_dirpath}
+
     tar --strip-components=1 --directory=${download_extract_dirpath} -xvzf ${download_filepath}
+
     returncode=$?
     if [[ $returncode != 0 ]]; then
         echo " Installation of OpenJDK with code $returncode failed."
         exit 1;
     fi
-    
 }
 
-download_and_extract_java ${OPENJDK_DOWNLOAD_URL_X64} ${OPENJDK_DOWNLOAD_SHA256_X64} "OpenJDK_X64"
-download_and_extract_java ${OPENJDK_DOWNLOAD_URL_ARM32} ${OPENJDK_DOWNLOAD_SHA256_ARM32} "OpenJDK_ARM32"
+function download_and_extract_zip
+{
+    local download_url=$1
+    local download_checksum=$2
+    local download_extract_dir=$3
 
-# Java X64: Linking
+    local download_filename="$(basename ${download_url})"
+    local download_filepath="${DL_DIR}/${download_filename}"
+    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
+
+    # Download and verify
+    download_and_verify $1 $2
+
+    # Unpack the downloaded tgz
+    mkdir -p ${download_extract_dirpath}
+
+    if [ -e $(which 7z) ]; then
+        7z x ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -o${BUILD_DIR}/openHAB
+        returncode=$?
+    else
+        unzip ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -d ${BUILD_DIR}/openHAB
+        returncode=$?
+    fi
+
+    returncode=$?
+    if [[ $returncode != 0 ]]; then
+        echo " Installation of OpenJDK with code $returncode failed."
+        exit 1;
+    fi
+}
+
+# Java X64
+download_and_extract_tgz ${OPENJDK_DOWNLOAD_URL_X64} ${OPENJDK_DOWNLOAD_CHECKSUM_X64} "OpenJDK_X64"
 ln -s ./OpenJDK_X64 ${BUILD_DIR}/OpenJDK_x86_64
 
-# Java ARM32: Linking
-
+# Java ARM32
+download_and_extract_tgz ${OPENJDK_DOWNLOAD_URL_ARM32} ${OPENJDK_DOWNLOAD_CHECKSUM_ARM32} "OpenJDK_ARM32"
+ln -s ./OpenJDK_X64 ${BUILD_DIR}/OpenJDK_armv5tel
 
 # Downloading openHAB
-[[ -e "${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document=${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} ${OPENHAB_DOWNLOAD_URL}
-returncode=$?
-if [[ $returncode != 0 ]]; then
-    echo " Downloading openHAB failed with code $returncode."
-    exit 1
-fi
-
-# Extract openHAB
-mkdir -p ${BUILD_DIR}/openHAB
-
-if [ -e $(which 7z) ]; then
-    7z x ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -o${BUILD_DIR}/openHAB
-    returncode=$?
-else
-    unzip ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -d ${BUILD_DIR}/openHAB
-    returncode=$?
-fi
-
-if [[ $returncode != 0 ]]; then
-    echo " Installation of openHAB failed with code $returncode."
-    exit 1;
-fi
+download_and_extract_zip ${OPENHAB_DOWNLOAD_URL} "NONE" "openHAB"
 
 # Adding little UI
 cp -rf ./ui ${BUILD_DIR}
@@ -100,7 +124,7 @@ cp -rf ./helper ${BUILD_DIR}
 
 tar --directory=${BUILD_DIR} -czvf ./package.tgz .
 
-FILE_NAME=openHAB-${OPENHAB_VERSION}-syno-noarch-0.001.spk
+FILE_NAME=openHAB-${OPENHAB_VERSION}-syno-noarch-${SPK_VERSION}.spk
 echo ${FILE_NAME}
 
 rm -f ${FILE_NAME}

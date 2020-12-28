35b718193b66013322a758afd76a523d75a9ef37
Thomas Karl Pietrowski <thopiekar@gmail.com>
2020-12-28 12:02:58 +0100

    build: Update script with separate build and package dir
    The package directory is removed after the tgz has been generated. Waste of time and the info is actually there in a different format.
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 [[ -z "${OPENHAB_VERSION:-}" ]] && echo "OPENHAB_VERSION is not set!" && exit 1
 [[ -z "${SPK_VERSION:-}" ]] && echo "SPK_VERSION is not set!" && exit 1
 
+# General
+BUILD_DIR=./build
+DL_DIR=./dl
+PACKAGE_DIR=./package
+
 # OpenJDK
 OPENJDK_DOWNLOAD_URL_X64="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_x64_linux_hotspot_11.0.9.1_1.tar.gz"
 OPENJDK_DOWNLOAD_CHECKSUM_X64="73ce5ce03d2efb097b561ae894903cdab06b8d58fbc2697a5abe44ccd8ecc2e5"
@@ -10,17 +15,9 @@ OPENJDK_DOWNLOAD_CHECKSUM_X64="73ce5ce03d2efb097b561ae894903cdab06b8d58fbc2697a5
 OPENJDK_DOWNLOAD_URL_ARM32="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jre_arm_linux_hotspot_11.0.9.1_1.tar.gz"
 OPENJDK_DOWNLOAD_CHECKSUM_ARM32="11628830c3c912edd10b91620ef0b9566640c5ea46439f1d028f3ebd98682dbb"
 
-# OpenHAB
+# openHAB
 OPENHAB_DOWNLOAD_URL="https://dl.bintray.com/openhab/mvn/org/openhab/distro/openhab/${OPENHAB_VERSION}/openhab-${OPENHAB_VERSION}.zip"
 OPENHAB_DOWNLOAD_CHECKSUM="NONE"
-OPENHAB_DOWNLOAD_FILENAME="$(basename ${OPENHAB_DOWNLOAD_URL})"
-
-# Ensure empty build directory
-DL_DIR=./dl
-BUILD_DIR=./build
-mkdir -p $DL_DIR
-mkdir -p ${BUILD_DIR}
-rm -rfv ${BUILD_DIR}/*
 
 function download_and_verify
 {
@@ -29,6 +26,9 @@ function download_and_verify
     
     local download_filename="$(basename ${download_url})"
     local download_filepath="${DL_DIR}/${download_filename}"
+    
+    # Ensure download directory
+    mkdir -p $DL_DIR
 
     # Download
     [[ -e "${download_filepath}" ]] || wget -nv --no-clobber --no-hsts --no-check-certificate --output-document="${download_filepath}" ${download_url}
@@ -57,7 +57,7 @@ function download_and_extract_tgz
 
     local download_filename="$(basename ${download_url})"
     local download_filepath="${DL_DIR}/${download_filename}"
-    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
+    local download_extract_dirpath="${PACKAGE_DIR}/${download_extract_dir}"
 
     # Download and verify
     download_and_verify $1 $2
@@ -82,7 +82,7 @@ function download_and_extract_zip
 
     local download_filename="$(basename ${download_url})"
     local download_filepath="${DL_DIR}/${download_filename}"
-    local download_extract_dirpath="${BUILD_DIR}/${download_extract_dir}"
+    local download_extract_dirpath="${PACKAGE_DIR}/${download_extract_dir}"
 
     # Download and verify
     download_and_verify $1 $2
@@ -91,10 +91,10 @@ function download_and_extract_zip
     mkdir -p ${download_extract_dirpath}
 
     if [ -e $(which 7z) ]; then
-        7z x ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -o${BUILD_DIR}/openHAB
+        7z x ${DL_DIR}/${download_filename} -o${PACKAGE_DIR}/openHAB
         returncode=$?
     else
-        unzip ${DL_DIR}/${OPENHAB_DOWNLOAD_FILENAME} -d ${BUILD_DIR}/openHAB
+        unzip ${DL_DIR}/${download_filename} -d ${PACKAGE_DIR}/openHAB
         returncode=$?
     fi
 
@@ -105,24 +105,57 @@ function download_and_extract_zip
     fi
 }
 
+#############################
+# Preparing build directory #
+#############################
+
+mkdir -p ${BUILD_DIR}
+rm -rfv ${BUILD_DIR}/*
+
+#########################
+# Preparing package.tgz #
+#########################
+
+mkdir -p ${PACKAGE_DIR}
+rm -rfv ${PACKAGE_DIR}/*
+
 # Java X64
 download_and_extract_tgz ${OPENJDK_DOWNLOAD_URL_X64} ${OPENJDK_DOWNLOAD_CHECKSUM_X64} "OpenJDK_X64"
-ln -s ./OpenJDK_X64 ${BUILD_DIR}/OpenJDK_x86_64
+ln -s ./OpenJDK_X64 ${PACKAGE_DIR}/OpenJDK_x86_64
 
 # Java ARM32
 download_and_extract_tgz ${OPENJDK_DOWNLOAD_URL_ARM32} ${OPENJDK_DOWNLOAD_CHECKSUM_ARM32} "OpenJDK_ARM32"
-ln -s ./OpenJDK_X64 ${BUILD_DIR}/OpenJDK_armv5tel
+ln -s ./OpenJDK_X64 ${PACKAGE_DIR}/OpenJDK_armv5tel
 
 # Downloading openHAB
 download_and_extract_zip ${OPENHAB_DOWNLOAD_URL} "NONE" "openHAB"
 
 # Adding little UI
-cp -rf ./ui ${BUILD_DIR}
+cp -rf ./ui ${PACKAGE_DIR}
 
 # Adding helper script(s)
-cp -rf ./helper ${BUILD_DIR}
+cp -rf ./helper ${PACKAGE_DIR}
 
-tar --directory=${BUILD_DIR} -czvf ./package.tgz .
+tar --directory=${PACKAGE_DIR} \
+    --file=${BUILD_DIR}/package.tgz \
+    -czv \
+    .
+
+rm -rfv ${PACKAGE_DIR}
+
+#######################
+# Preparing final SPK #
+#######################
+
+cp -rfv \
+    conf \
+    scripts \
+    WIZARD_UIFILES \
+    INFO \
+    LICENSE \
+    NOTICE \
+    PACKAGE_ICON*.PNG \
+    ${BUILD_DIR}
 
 FILE_NAME=openHAB-${OPENHAB_VERSION}-syno-noarch-${SPK_VERSION}.spk
 echo ${FILE_NAME}

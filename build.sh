b442446110b8497758a5d561ea3183e15c6d2c50
Thomas Karl Pietrowski <thopiekar@gmail.com>
2021-01-10 22:17:45 +0100

    build: Moving package version into a separate file
    Whenever we change something to our packaging files, this version shall be bumped.
    It shall indicate the state of the scripts inside our packages.
    
    E.g. we can update older packages with newer packaging files to fix issues on upgrading or whatever.
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 [[ -z "${OPENHAB_VERSION:-}" ]] && echo "OPENHAB_VERSION is not set!" && exit 1
-[[ -z "${SPK_VERSION:-}" ]] && echo "SPK_VERSION is not set!" && exit 1
+SPK_VERSION=$(cat ./PKGVERSION)
 
 # General
 BUILD_DIR=./build

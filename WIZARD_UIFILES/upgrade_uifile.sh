22ec0581f21ab1038ee8d638d7879717343cdc92
Thomas Karl Pietrowski <thopiekar@gmail.com>
2021-01-10 16:28:37 +0100

    WIZARD_UIFILES: Second attempt to make it working
    .. however, not working as expected. Will be reverted later.
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- WIZARD_UIFILES/upgrade_uifile.sh
+++ WIZARD_UIFILES/upgrade_uifile.sh
@@ -4,11 +4,15 @@ SYNOPKG_PKGDEST_OPENHAB_ENV="${SYNOPKG_PKGDEST}/${SYNOPKG_PKGNAME}.env"
 
 [[ -f ${SYNOPKG_PKGDEST_OPENHAB_ENV} ]] && source ${SYNOPKG_PKGDEST_OPENHAB_ENV}
 
-cat `dirname $0`/install_uifile.in \
-    | sed -e "s/%OPENHAB_HTTP_ADDRESS%/{OPENHAB_HTTP_ADDRESS:-0.0.0.0}/g"
-    | sed -e "s/%OPENHAB_HTTP_ADDRESS_DEFAULT%/0.0.0.0/g"
-    | sed -e "s/%OPENHAB_HTTP_PORT%/{OPENHAB_HTTP_PORT:-8080}/g"
-    | sed -e "s/%OPENHAB_HTTP_PORT_DEFAULT%/8080/g"
-    | sed -e "s/%OPENHAB_HTTPS_PORT%/{OPENHAB_HTTPS_PORT:-8443}/g"
-    | sed -e "s/%OPENHAB_HTTPS_PORT_DEFAULT%/8443/g"
-    > `dirname $0`/install_uifile
+cat `dirname $0`/upgrade_uifile.in \
+    | sed -e "s/%OPENHAB_HTTP_ADDRESS%/${OPENHAB_HTTP_ADDRESS:-0.0.0.0}/g" \
+    | sed -e "s/%OPENHAB_HTTP_ADDRESS_DEFAULT%/0.0.0.0/g" \
+    | sed -e "s/%OPENHAB_HTTP_PORT%/${OPENHAB_HTTP_PORT:-8080}/g" \
+    | sed -e "s/%OPENHAB_HTTP_PORT_DEFAULT%/8080/g" \
+    | sed -e "s/%OPENHAB_HTTPS_PORT%/${OPENHAB_HTTPS_PORT:-8443}/g" \
+    | sed -e "s/%OPENHAB_HTTPS_PORT_DEFAULT%/8443/g" \
+    > `dirname $0`/upgrade_uifile
+
+cat `dirname $0`/upgrade_uifile
+
+exit 0

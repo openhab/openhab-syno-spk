2741ea9b02c326b17dafb34b8d3cbecc62b0bc7a
Thomas Karl Pietrowski <thopiekar@gmail.com>
2021-03-27 13:08:25 +0100

    build: Hotfix on generated package version
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com>

--- build.sh
+++ build.sh
@@ -166,7 +166,7 @@ cp -rfv \
 cp -rfv \
     INFO.in \
     ${BUILD_DIR}/INFO
-sed -i -e "s/#PACKAGE_VERSION#/${OPENHAB_VERSION}-${SPK_VERSION}/g" ${BUILD_DIR}/INFO
+sed -i -e "s/#PACKAGE_VERSION#/${OPENHAB_VERSION}.${SPK_VERSION}/g" ${BUILD_DIR}/INFO
 
 FILE_NAME=openHAB-${OPENHAB_VERSION}-syno-noarch-${SPK_VERSION}.spk
 echo ${FILE_NAME}
